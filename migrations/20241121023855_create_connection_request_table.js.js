d945e964190b670f42d84f666ef69eb10f9b45b8
Tanya Vezyr <tetiana.vezyr@gmail.com>
2024-11-20 20:41:23 -0700

    fix connection_request migration

--- migrations/20241121023855_create_connection_request_table.js.js
+++ migrations/20241121023855_create_connection_request_table.js.js
@@ -2,40 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-
 export function up(knex) {
   return knex.schema.createTable("connection_request", (table) => {
     table.increments("id").primary();
     table
-      .integer("sender_id")
-      .unsigned()
-      .references("id")
-      .inTable("user")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
-    table;
+    .integer("sender_id")
+    .unsigned()
+    .references("id")
+    .inTable("user")
+    .onUpdate("CASCADE")
+    .onDelete("CASCADE");
     table
-      .integer("receiver_id")
-      .unsigned()
-      .references("id")
-      .inTable("user")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
+    .integer("receiver_id")
+    .unsigned()
+    .references("id")
+    .inTable("user")
+    .onUpdate("CASCADE")
+    .onDelete("CASCADE");
     table.enu("status", ["accepted", "pending", "declined"]).notNullable();
-
-    table
-      .string("sorted_sender_receiver")
-      .stored()
-      .as(
-        knex.raw(
-          'CONCAT(LEAST(sender_id, receiver_id), "_", GREATEST(sender_id, receiver_id))'
-        )
-      );
-    table.unique("sorted_sender_receiver");
-    table.timestamp("created_at").defaultTo(knex.fn.now());
-    table
-      .timestamp("updated_at")
-      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    table.string("sorted_sender_receiver").notNullable().unique();
+    table.timestamps(true, true);
   });
 }
 
@@ -43,6 +29,6 @@ export function up(knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export function down (knex) {
+export function down(knex) {
   return knex.schema.dropTableIfExists("connection_request");
-};
+}
